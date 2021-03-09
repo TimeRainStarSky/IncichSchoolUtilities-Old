11ac7c5eecf74babcdf6b616924077aed43e898b
TimeRainStarSky <63490117+TimeRainStarSky@users.noreply.github.com>
2021-03-10 02:00:26 +0800

    删除测试消息
    由于我经常启动和停止程序，而这会产生大量无用消息，所以删除

--- incich_school_utilities/incich_api/__init__.py
+++ incich_school_utilities/incich_api/__init__.py
@@ -6,7 +6,7 @@ import random
 from urllib.parse import unquote_plus
 
 
-logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
+logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s：%(message)s',
                     level=logging.INFO)
 
 
@@ -49,7 +49,7 @@ class IncichStudent:
             req = requests.post(url, params=params, headers=headers, data=data, timeout=timeout)
 
             if req.status_code != 200:
-                logging.warning("HTTP状态码" + str(req.status_code) + ": " + str(req.content.decode("utf-8")))
+                logging.warning("HTTP状态码" + str(req.status_code) + ":" + str(req.content.decode("utf-8")))
                 time.sleep(0.5)
                 self.token = self.get_token()
                 return self.post(url, params=params, headers=headers, timeout=timeout)
@@ -96,7 +96,7 @@ class IncichStudent:
             req = requests.get(url, params=params, headers=headers, timeout=timeout)
 
             if req.status_code != 200:
-                logging.warning("HTTP状态码" + str(req.status_code) + ": " + str(req.content.decode("utf-8")))
+                logging.warning("HTTP状态码" + str(req.status_code) + ":" + str(req.content.decode("utf-8")))
                 time.sleep(0.5)
                 self.token = self.get_token()
                 return self.get(url, params=params, headers=headers, timeout=timeout)
@@ -112,11 +112,11 @@ class IncichStudent:
     def get_token(self):
         logging.info("获取Token")
         req = self.post(self.api_url + "/oauth/token", data={
-            "systemid": "parent",
-            "grant_type": "password",
-            "username": self.union_id
+            "systemid":"parent",
+            "grant_type":"password",
+            "username":self.union_id
         }, headers={
-            "Authorization": "Basic aW5jaF9wYXJlbnQ6ODVhMzNlNTAtMmJmZC0xMWU4LTkzYzktMzhjOTg2NDEyZmZj"
+            "Authorization":"Basic aW5jaF9wYXJlbnQ6ODVhMzNlNTAtMmJmZC0xMWU4LTkzYzktMzhjOTg2NDEyZmZj"
         }).json()
         logging.info(req)
         return req['access_token']
@@ -124,29 +124,29 @@ class IncichStudent:
     # 注册账号
     def register(self):
 
-        logging.info("注册账号...")
+        logging.info("注册账号……")
         req = self.post(self.api_url + "/ThirdLogin/login.json", params={
-            "unionid": self.union_id,
-            "nickname": str(random.randint(10000, 1000000)),
-            "source": "QQ"
+            "unionid":self.union_id,
+            "nickname":str(random.randint(10000, 1000000)),
+            "source":"QQ"
         }).json()
         logging.info(req)
 
         req = self.post(self.api_url + "/ThirdLogin/saveToken.json", params={
-            "unionid": self.union_id,
+            "unionid":self.union_id,
         }).json()
         logging.info(req)
 
     # 绑定学生
     def bind(self, class_id, grade_id, school_id, relation_name, phone):
         req = self.post(self.api_url + "/bindStudent/bind.json", params={
-            "unionid": self.union_id,
-            "name": self.student_name,
-            "classid": class_id,
-            "schoolid": school_id,
-            "gradeid": grade_id,
-            "relationname": relation_name,
-            "phone": phone
+            "unionid":self.union_id,
+            "name":self.student_name,
+            "classid":class_id,
+            "schoolid":school_id,
+            "gradeid":grade_id,
+            "relationname":relation_name,
+            "phone":phone
         }).json()
         logging.info(req)
         return req
@@ -154,17 +154,17 @@ class IncichStudent:
     # 获取邀请码信息
     def get_class_by_code(self, code):
         req = self.get(self.api_url + "/bindStudent/getClass.json", params={
-            "unionid": self.union_id,
-            "invitecode": code
+            "unionid":self.union_id,
+            "invitecode":code
         }).json()
         logging.info(req)
         return req
 
     # 获取已绑定的学生
     def get_student(self):
-        logging.info("获取账号信息...")
+        logging.info("获取账号信息……")
         req = self.get(self.api_url + "/bindStudent/getBindInfo.json", params={
-            "unionid": self.union_id
+            "unionid":self.union_id
         }).json()
         logging.info(req)
         return req
@@ -177,20 +177,20 @@ class IncichStudent:
             self.send_msg(msg[800:])
             return None
 
-        logging.info("发送的消息: " + msg)
+        logging.info("发送的消息：" + msg)
 
         req = self.post(self.api_url + "/message/save", params={
-            "classid": self.class_id,
-            "schoolid": self.school_id,
-            "gradeid": self.grade_id,
-            "adduser": self.union_id,
-            "addusername": "IncichRobot",
-            "type": 1,
-            "stuname": self.student_name,
-            "stuguid": self.student_guid,
-            "url": "",
-            "voicelen": 0,
-            "msg": msg
+            "classid":self.class_id,
+            "schoolid":self.school_id,
+            "gradeid":self.grade_id,
+            "adduser":self.union_id,
+            "addusername":"IncichRobot",
+            "type":1,
+            "stuname":self.student_name,
+            "stuguid":self.student_guid,
+            "url":"",
+            "voicelen":0,
+            "msg":msg
         }).json()
         logging.info(req)
         self.msg_processed.append(req['guid'])
@@ -199,11 +199,11 @@ class IncichStudent:
     # 读取所有消息
     def read_msg_all(self):
         req = self.post(self.api_url + "/getInfo/getNotice.json", params={
-            "unionid": self.union_id,
-            "classid": self.class_id,
-            "name": self.student_name,
-            "pageno": 1,
-            "length": 20
+            "unionid":self.union_id,
+            "classid":self.class_id,
+            "name":self.student_name,
+            "pageno":1,
+            "length":20
         }).json()
         # logging.info(req)
         return req
@@ -215,30 +215,30 @@ class IncichStudent:
                 for i in self.read_msg_all()['data']:
                     if self.student_name in i['addusername'] and i['guid'] not in self.msg_processed:
                         self.msg_processed.append(i['guid'])
-                        logging.info("收到新消息: " + str(unquote_plus(i['title'])))
+                        logging.info("收到新消息：" + str(unquote_plus(i['title'])))
                         return unquote_plus(i['title'])
                 time.sleep(2)
             except Exception as e:
-                logging.warning("获取消息时出错: " + str(e))
+                logging.warning("获取消息时出错：" + str(e))
 
     # 发送一条语音消息
     def send_sound_msg(self, input_file):
         res = requests.post('http://school.incich.com:9207/UploadImageServlet', files={
-            "file": input_file
+            "file":input_file
         }).json()
         logging.info(res)
         req = self.post(self.api_url + "/message/save", params={
-            "classid": self.class_id,
-            "schoolid": self.school_id,
-            "gradeid": self.grade_id,
-            "adduser": self.union_id,
-            "addusername": "IncichRobot",
-            "type": 2,
-            "stuname": self.student_name,
-            "stuguid": self.student_guid,
-            "url": res['url'],
-            "voicelen": 666666,
-            "msg": ""
+            "classid":self.class_id,
+            "schoolid":self.school_id,
+            "gradeid":self.grade_id,
+            "adduser":self.union_id,
+            "addusername":"IncichRobot",
+            "type":2,
+            "stuname":self.student_name,
+            "stuguid":self.student_guid,
+            "url":res['url'],
+            "voicelen":666666,
+            "msg":""
         }).json()
         logging.info(req)
         self.msg_processed.append(req['guid'])
@@ -247,21 +247,21 @@ class IncichStudent:
     # 发送一条视频消息
     def send_video_msg(self, input_file):
         res = requests.post('http://school.incich.com:9207/UploadImageServlet', files={
-            "file": input_file
+            "file":input_file
         }).json()
         logging.info(res)
         req = self.post(self.api_url + "/message/save", params={
-            "classid": self.class_id,
-            "schoolid": self.school_id,
-            "gradeid": self.grade_id,
-            "adduser": self.union_id,
-            "addusername": "IncichRobot",
-            "type": 3,
-            "stuname": self.student_name,
-            "stuguid": self.student_guid,
-            "url": res['url'],
-            "voicelen": 0,
-            "msg": "%25E7%25BB%2599%25E6%2582%25A8%25E5%258F%2591%25E6%259D%25A5%25E4%25B8%2580%25E6%259D%25A1%25E8%25A7%2586%25E9%25A2%2591%25E6%25B6%2588%25E6%2581%25AF"
+            "classid":self.class_id,
+            "schoolid":self.school_id,
+            "gradeid":self.grade_id,
+            "adduser":self.union_id,
+            "addusername":"IncichRobot",
+            "type":3,
+            "stuname":self.student_name,
+            "stuguid":self.student_guid,
+            "url":res['url'],
+            "voicelen":0,
+            "msg":"%25E7%25BB%2599%25E6%2582%25A8%25E5%258F%2591%25E6%259D%25A5%25E4%25B8%2580%25E6%259D%25A1%25E8%25A7%2586%25E9%25A2%2591%25E6%25B6%2588%25E6%2581%25AF"
         }).json()
         logging.info(req)
         self.msg_processed.append(req['guid'])
@@ -270,28 +270,28 @@ class IncichStudent:
     # 发送一条图片消息
     def send_image_msg(self, input_file):
         res = requests.post('http://school.incich.com:9207/UploadImageServlet', files={
-            "file": input_file
+            "file":input_file
         }).json()
         logging.info(res)
         req = self.post(self.api_url + "/message/save", params={
-            "classid": self.class_id,
-            "schoolid": self.school_id,
-            "gradeid": self.grade_id,
-            "adduser": self.union_id,
-            "addusername": "IncichRobot",
-            "type": 4,
-            "stuname": self.student_name,
-            "stuguid": self.student_guid,
-            "url": res['url'],
-            "voicelen": 0,
-            "msg": "%25E7%25BB%2599%25E6%2582%25A8%25E5%258F%2591%25E6%259D%25A5%25E4%25B8%2580%25E6%259D%25A1%25E5%259B%25BE%25E7%2589%2587%25E6%25B6%2588%25E6%2581%25AF"
+            "classid":self.class_id,
+            "schoolid":self.school_id,
+            "gradeid":self.grade_id,
+            "adduser":self.union_id,
+            "addusername":"IncichRobot",
+            "type":4,
+            "stuname":self.student_name,
+            "stuguid":self.student_guid,
+            "url":res['url'],
+            "voicelen":0,
+            "msg":"%25E7%25BB%2599%25E6%2582%25A8%25E5%258F%2591%25E6%259D%25A5%25E4%25B8%2580%25E6%259D%25A1%25E5%259B%25BE%25E7%2589%2587%25E6%25B6%2588%25E6%2581%25AF"
         }).json()
         logging.info(req)
         self.msg_processed.append(req['guid'])
         return req
 
     def __init__(self, union_id, student_name, invite_code):
-        logging.info("正在初始化...")
+        logging.info("正在初始化……")
         self.union_id = union_id
         self.student_name = student_name
         self.invite_code = invite_code
@@ -312,7 +312,7 @@ class IncichStudent:
                 self.student_guid = i['guid']
                 break
         else:
-            logging.info("学生未绑定.正在尝试绑定.")
+            logging.info("学生未绑定，正在尝试绑定")
 
             # 随机生成手机号
             # 第二位数字
@@ -320,11 +320,11 @@ class IncichStudent:
 
             # 第三位数字
             third = {
-                3: random.randint(0, 9),
-                4: [5, 7, 9][random.randint(0, 2)],
-                5: [i for i in range(10) if i != 4][random.randint(0, 8)],
-                7: [i for i in range(10) if i not in [4, 9]][random.randint(0, 7)],
-                8: random.randint(0, 9),
+                3:random.randint(0, 9),
+                4:[5, 7, 9][random.randint(0, 2)],
+                5:[i for i in range(10) if i != 4][random.randint(0, 8)],
+                7:[i for i in range(10) if i not in [4, 9]][random.randint(0, 7)],
+                8:random.randint(0, 9),
             }[second]
 
             # 最后八位数字
@@ -333,23 +333,18 @@ class IncichStudent:
             bind_res = self.bind(self.class_id, self.grade_id, self.school_id,
                                  "IncichRobot", "1{}{}{}".format(second, third, suffix))
             if not bind_res['success']:
-                raise Exception("绑定学生失败: " + str(bind_res))
+                raise Exception("绑定学生失败：" + str(bind_res))
             self.student_guid = bind_res['guid']
 
-        logging.info("学生GUID: " + self.student_guid)
+        logging.info("学生GUID：" + self.student_guid)
 
-        logging.info("尝试读取所有消息...")
+        logging.info("尝试读取所有消息……")
         all_msg = self.read_msg_all()
 
         self.msg_processed = []
         for i in all_msg['data']:
             if self.student_name in i['addusername']:
                 self.msg_processed.append(i['guid'])
-        logging.info("消息GUID: " + str(self.msg_processed))
+        logging.info("消息GUID：" + str(self.msg_processed))
 
-        logging.info("尝试发送文本消息...")
-        msg_res = self.send_msg("Incich School v3-Beta API 初始化成功.")
-        if not msg_res['success']:
-            raise Exception("发送消息失败: " + str(msg_res))
-
-        logging.info("初始化完成!")
+        logging.info("Incich School v3-Beta API 初始化完成!")
